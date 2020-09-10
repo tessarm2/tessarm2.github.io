45c2bdd48a642c507ce329f1e7e92909c161fd14
tessarm2 <tessarm2@illinois.edu>
2020-09-10 14:23:13 -0500

    Add Gradient Background
    added gradient background and cloned onto desktop

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,13 @@
       document.onmousemove = function() {
         var x = event.clientX *100 / window.innerWidth + "%";
         var y = event.clientY *100 / window.innerHeight + "%";
+        var x1 = event.clientX/window.innerWidth;
+        var y2 = event.clientY/window.innerHeight;
+        var pct = 360*(Math.sqrt(x1*x1 + y2*y2));
+        document.body.style.background = "linear-gradient(" + pct + "deg,#FA709A,#795bb0)";
         for (var i = 0; i < 2; i++) {
           balls[i].style.left = x;
           balls[i].style.top = y;
           balls[i].style.transform = "translate(- "+x+", "+y+")";       
          }
       }
-      
